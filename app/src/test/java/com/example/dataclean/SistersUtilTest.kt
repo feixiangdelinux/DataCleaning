483ae7ccc679a95bb5bfd9651077c28a54e94458
95749 <linux2005@163.com>
2020-09-18 15:39:49 +0800

    11111111111

--- app/src/test/java/com/example/dataclean/SistersUtilTest.kt
+++ app/src/test/java/com/example/dataclean/SistersUtilTest.kt
@@ -16,39 +16,61 @@ import org.junit.Test
  * date : 20-7-13 下午3:29
  */
 class SistersUtilTest {
-
     /**
      * A完整数据并去重复/home/ccg
      */
     @Test
     fun cleaningData() {
+        val startTime = System.currentTimeMillis()
         //1加载json文件到内存中
-        val fileStr = KtStringUtil.getStrInFile("/home/ccg/sisters.json")
+//        val fileStr = KtStringUtil.getStrInFile("/home/ccg/sisters.json")
+        val fileStr = KtStringUtil.getStrInFile("E:\\sisters.json")
         //2把json转换成list
-        val listDatas = GsonBuilder().create()
-            .fromJson<List<VideoBean>>(fileStr, object : TypeToken<List<VideoBean>>() {}.type)
-        println("一共: " + listDatas.size)
-        //3把不完整的数据填写完整
-        for ((aa, av) in listDatas.withIndex()) {
-            for ((bb, bv) in listDatas.withIndex()) {
-                if (av.id != bv.id
-                    && av.name == bv.name
-                    && av.getpUrl().isNullOrEmpty()
-                    && av.getvUrl().isNotEmpty()
-                    && bv.getvUrl().isNullOrEmpty()
-                    && bv.getpUrl().isNotEmpty()
-                ) {
-                    av.setpUrl(bv.getpUrl())
-                    av.tags = bv.tags
-                    bv.setvUrl(av.getvUrl())
-                }
+        val listDatas = GsonBuilder().create().fromJson<ArrayList<VideoBean>>(
+            fileStr,
+            object : TypeToken<ArrayList<VideoBean>>() {}.type
+        )
+        println("原始数据一共: " + listDatas.size)
+        //3原始数据去重复
+        val setOne = LinkedHashSet<VideoBean>()
+        val listOne = ArrayList<VideoBean>()
+        setOne.addAll(listDatas)
+        listOne.addAll(setOne)
+        listDatas.clear()
+        println("原始数据去重后一共: " + listOne.size)
+        //4对数据进行分组，一组视频地址，一组缩略图
+        val listVideo = ArrayList<VideoBean>()
+        val listThree = ArrayList<VideoBean>()
+        for (videoUrlData in listOne) {
+            if (videoUrlData.getvUrl().isNotEmpty()) {
+                listVideo.add(videoUrlData)
+            } else if (videoUrlData.getpUrl().isNotEmpty()) {
+                listThree.add(videoUrlData)
             }
         }
-        //4对对象进行去重操作
+        listOne.clear()
+        println("视频地址一共: " + listVideo.size)
+        //5把缩略图合并到视频地址中
+        for (index in listVideo.indices) {
+            for (pictureUrlData in listThree) {
+                if (listVideo[index].id != pictureUrlData.id && listVideo[index].name == pictureUrlData.name) {
+                    listVideo[index].setpUrl(pictureUrlData.getpUrl())
+                    listVideo[index].tags = pictureUrlData.tags
+                    continue
+                }
+            }
+            if (index % 10000 == 0) {
+                println("当前遍历到第: $index")
+            }
+        }
+        listThree.clear()
+        //6对完整的数据进行去重操作
         val set = LinkedHashSet<VideoBean>()
         val list = ArrayList<VideoBean>()
-        set.addAll(listDatas)
+        set.addAll(listVideo)
         list.addAll(set)
+        listVideo.clear()
+        //7去掉不完整的数据
         val iterator = list.iterator()
         while (iterator.hasNext()) {
             val item = iterator.next()
@@ -56,18 +78,24 @@ class SistersUtilTest {
                 iterator.remove()
             }
         }
-        //5把去重复的数据保存到文件中
+        //8把去重复的数据保存到文件中
         println("去重复后: " + list.size)
-        KtStringUtil.saveAsFileWriter("/home/ccg/sisters1.json", GsonBuilder().create().toJson(list))
+//        KtStringUtil.saveAsFileWriter("/home/ccg/sisters1.json", GsonBuilder().create().toJson(list))
+        KtStringUtil.saveAsFileWriter("E:\\sisters1.json", GsonBuilder().create().toJson(list))
+        val endTime = System.currentTimeMillis()
+        println("耗时：  " + (endTime - startTime) / 1000 / 60 + " 分钟")
     }
+
     /**
      * B1把不能播放的数据删除并把最终结果保存到本地
      */
     @Test
     fun cleaningDataTwo() {
         //1加载json文件到内存中
-        val fileStr = KtStringUtil.getStrInFile("/home/ccg/sisters1.json")
-        val fileStrTwo = KtStringUtil.getStrInFile("/home/ccg/sistertext.json")
+//        val fileStr = KtStringUtil.getStrInFile("/home/ccg/sisters1.json")
+        val fileStr = KtStringUtil.getStrInFile("E:\\sisters1.json")
+//        val fileStrTwo = KtStringUtil.getStrInFile("/home/ccg/sistertext.json")
+        val fileStrTwo = KtStringUtil.getStrInFile("E:\\sistertext.json")
 //        //2把json转换成list
         val listDatasOne = GsonBuilder().create()
             .fromJson<ArrayList<VideoBean>>(
@@ -76,10 +104,10 @@ class SistersUtilTest {
             )
         val listDatasTwo = GsonBuilder().create()
             .fromJson<List<VideoInfo>>(fileStrTwo, object : TypeToken<List<VideoInfo>>() {}.type)
-        for ((aa, av) in listDatasOne.withIndex()) {
-            for ((bb, bv) in listDatasTwo.withIndex()) {
-                if (av.getvUrl() == bv.getvUrl()) {
-                    av.i = "1"
+        for (videoUrlData in listDatasOne) {
+            for (pictureUrlData in listDatasTwo) {
+                if (videoUrlData.getvUrl() == pictureUrlData.getvUrl()) {
+                    videoUrlData.i = "1"
                 }
             }
         }
@@ -91,10 +119,8 @@ class SistersUtilTest {
             }
         }
         println("最终的: " + listDatasOne.size)
-        KtStringUtil.saveAsFileWriter(
-            "/home/ccg/sisterok.json",
-            GsonBuilder().create().toJson(listDatasOne)
-        )
+//        KtStringUtil.saveAsFileWriter("/home/ccg/sistersok.json", GsonBuilder().create().toJson(listDatasOne))
+        KtStringUtil.saveAsFileWriter("E:\\sistersok.json", GsonBuilder().create().toJson(listDatasOne))
     }
 
     /**
@@ -103,33 +129,63 @@ class SistersUtilTest {
     @Test
     fun cleaningDataThree() {
         //1加载json文件到内存中
-        val fileStr = KtStringUtil.getStrInFile("/home/ccg/sisterok.json")
-        val listDatasOne = GsonBuilder().create()
-            .fromJson<ArrayList<VideoBean>>(
-                fileStr,
-                object : TypeToken<ArrayList<VideoBean>>() {}.type
-            )
-        val zongList = KtStringUtil.averageAssign(listDatasOne, 3)
-        val aa = FinalVideoBean()
-        aa.timeStamp = System.currentTimeMillis()
-        aa.data = zongList!![0]
-        KtStringUtil.saveAsFileWriter(
-            "/home/ccg/sister1.json",
-            GsonBuilder().create().toJson(aa)
+//        val fileStr = KtStringUtil.getStrInFile("/home/ccg/sistersok.json")
+        val fileStr = KtStringUtil.getStrInFile("E:\\sistersok.json")
+        val listDatasOne = GsonBuilder().create().fromJson<ArrayList<VideoBean>>(
+            fileStr,
+            object : TypeToken<ArrayList<VideoBean>>() {}.type
         )
-        val bb = FinalVideoBean()
-        bb.timeStamp = System.currentTimeMillis()
-        bb.data = zongList[1]
-        KtStringUtil.saveAsFileWriter(
-            "/home/ccg/sister2.json",
-            GsonBuilder().create().toJson(bb)
-        )
-        val cc = FinalVideoBean()
-        cc.timeStamp = System.currentTimeMillis()
-        cc.data = zongList[2]
-        KtStringUtil.saveAsFileWriter(
-            "/home/ccg/sister3.json",
-            GsonBuilder().create().toJson(cc)
-        )
+        val videoTag: MutableList<String> = ArrayList()
+        val videoUrl: MutableList<String> = ArrayList()
+        for (i in listDatasOne) {
+            if (!videoTag.contains(i.tags)) {
+                videoTag.add(i.tags)
+            }
+        }
+        for (i in videoTag.indices) {
+            val tempList: MutableList<VideoBean> = ArrayList()
+            for (j in listDatasOne) {
+                if (videoTag[i] == j.tags) {
+                    tempList.add(j)
+                }
+            }
+            val ssss = VideoListBean()
+            ssss.videoTag = videoTag[i]
+            ssss.data = tempList
+//            val videoU = "/home/ccg/$i.json"
+            val videoU = "E:\\新建文件夹\\$i.json"
+            KtStringUtil.saveAsFileWriter(videoU, GsonBuilder().create().toJson(ssss))
+            videoUrl.add("https://siyou.nos-eastchina1.126.net/21/sisters/$i.json")
+        }
+        val secon = SecondListBean()
+        secon.videoTag = videoTag
+        secon.videoUrl = videoUrl
+//        KtStringUtil.saveAsFileWriter("/home/ccg/index.json", GsonBuilder().create().toJson(secon))
+        KtStringUtil.saveAsFileWriter("E:\\新建文件夹\\index.json", GsonBuilder().create().toJson(secon))
     }
+
+
+    /**
+     * B2把最终结果分解成几份
+     */
+    @Test
+    fun cleaningDataFour() {
+        //1加载json文件到内存中
+//        val fileStr = KtStringUtil.getStrInFile("/home/ccg/sisters.json")
+        val fileStr = KtStringUtil.getStrInFile("E:\\sisters.json")
+        val listDatasOne = GsonBuilder().create().fromJson<ArrayList<VideoBean>>(
+            fileStr,
+            object : TypeToken<ArrayList<VideoBean>>() {}.type
+        )
+        val videoTag: MutableList<String> = ArrayList()
+        for (i in listDatasOne) {
+            if (!videoTag.contains(i.tags)) {
+                videoTag.add(i.tags)
+            }
+        }
+        for (i in videoTag) {
+            print(i)
+        }
+    }
+
 }
